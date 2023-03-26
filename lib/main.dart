6a30bdf2ff2a67729c54c76d94cb1824d09af72e
Syful Sharif <sharif.syful@gmail.com>
2023-03-26 12:41:07 +0600

    Flutter cards added

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,45 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 15.0,
                     fontFamily: 'Poppins'),
               ),
+              SizedBox(
+                width: 150.0,
+                height: 35.0,
+                child: Divider(color: Colors.white),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 25.0),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text(
+                      '+880 1777 7777 77',
+                      style: TextStyle(
+                          fontFamily: 'Titillium Web',
+                          color: Colors.teal.shade900,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text(
+                      'mail@sharif.live',
+                      style: TextStyle(
+                          fontFamily: 'Titillium Web',
+                          color: Colors.teal.shade900,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),

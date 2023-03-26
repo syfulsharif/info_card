23a61d11b1cd56b42953aba6948d8b766a74401b
Syful Sharif <sharif.syful@gmail.com>
2023-03-26 12:56:01 +0600

    Font Size in card items updated

--- lib/main.dart
+++ lib/main.dart
@@ -41,15 +41,16 @@ class MyApp extends StatelessWidget {
                 child: Divider(color: Colors.white),
               ),
               Card(
-                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 25.0),
+                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
                 child: Padding(
                   padding:
-                      EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
                   child: ListTile(
                     leading: Icon(Icons.phone),
                     title: Text(
                       '+880 1777 7777 77',
                       style: TextStyle(
+                          fontSize: 20.0,
                           fontFamily: 'Titillium Web',
                           color: Colors.teal.shade900,
                           fontWeight: FontWeight.w700),
@@ -58,16 +59,17 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Card(
-                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
                 child: Padding(
                   padding:
-                      EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
                   child: ListTile(
                     leading: Icon(Icons.email),
                     title: Text(
                       'mail@sharif.live',
                       style: TextStyle(
                           fontFamily: 'Titillium Web',
+                          fontSize: 20.0,
                           color: Colors.teal.shade900,
                           fontWeight: FontWeight.w700),
                     ),
